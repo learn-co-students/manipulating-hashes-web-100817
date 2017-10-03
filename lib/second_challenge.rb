6bd5b82046aa2a1b82cc930f2a9c58c3b5860bcd
Brandon  Harris <cheeridon@gmail.com>
2017-10-03 19:26:18 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,13 @@
-def second_challenge(groceries)
+#groceries = {
+#  dairy:
+#}
+
+def second_challenge
+  groceries = {
+  :dairy => ["milk", "yogurt", "cheese"],
+  :vegetables => ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"],
+   grains: ["rice", "pasta"]}
+
   groceries.values.flatten
 end
