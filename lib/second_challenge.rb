2820ab521d95d331ec8a826f41cf54e8e7015f87
tohanian <tadehohanian@gmail.com>
2017-10-05 06:43:29 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,12 @@ def second_challenge
   }
 
   #code your solution here!
+  grocery_items = []
+  groceries.each do |food_group, foods|
+    foods.each do |food|
+      grocery_items << food
+    end
+  end
 
-  
-
+  return grocery_items
 end
